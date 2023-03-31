957fd9b2c22b0e0fc8db53c73c3f442b8e0cf2e1
mehboobpatel <92875632+mehboobpatel@users.noreply.github.com>
2023-03-31 16:59:25 +0530

    database update

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
 from gtts import gTTS
 import os
-import subprocess
+import pyodbc
 
 app = Flask(__name__)
+
+# Connect to Azure SQL DB
+server = 'boomlet.database.windows.net'
+database = 'boomlet'
+username = 'maddy'
+password = 'Paneer@1234'
+driver = '{ODBC Driver 17 for SQL Server}'
+cnxn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -16,13 +25,28 @@ def submit():
     purpose = request.form.get('purpose')
     number = request.form.get('number')
     welcome_message = f"Welcome {full_name} to BoomLet Media"
-    
+
+    # Insert user details into boomlet table
+    cursor = cnxn.cursor()
+    insert_query = f"INSERT INTO boomlet (full_name, purpose, number) VALUES ('{full_name}', '{purpose}', '{number}')"
+    cursor.execute(insert_query)
+    cnxn.commit()
+
     # Generate audio file using gTTS
     tts = gTTS(welcome_message)
     tts.save(f"static/{full_name}.mp3")
-    
+
     return render_template('success.html', full_name=full_name, purpose=purpose, number=number)
 
+@app.route('/database', methods=['GET'])
+def database():
+    # Select all records from boomlet table
+    cursor = cnxn.cursor()
+    select_query = "SELECT * FROM boomlet"
+    cursor.execute(select_query)
+    records = cursor.fetchall()
+
+    return render_template('database.html', records=records)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5500)
+    app.run(debug=True, port=5500)
